8688c488dc8701f15e0008f204a1e12c0e65cbfc
Arlindo Correia Junior <arlindo.junior@totvs.com.br>
2019-08-11 18:57:06 -0300

    Adicionar arquivos de projeto.

---
+++ MV_Busca/routes/ordena.js
@@ -0,0 +1,19 @@
+﻿
+const express = require('express');
+const router = express.Router();
+
+function ordenaDATA() {
+
+    Console.log('Passou aqui!!!');
+
+}
+
+
+module.exports = router;
+
+
+
+/* GET home page. */
+router.get('/ordena', function (req, res) {
+    res.render(ordena, {data: ordenaDATA() });
+});
